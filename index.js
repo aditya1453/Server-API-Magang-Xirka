2cfbf861d201f710c1c7e60c3d3c3ab13f87549c
Dimas Yoga <dimasyogapra@gmail.com>
2019-06-14 13:45:05 +0700

    add PUT Method

--- index.js
+++ index.js
@@ -19,12 +19,14 @@ app.get('/', (request, response) => {
 app.get('/card', db.getCard)
 app.post('/card', db.createCard)
 app.get('/card/:id', db.getCardById)
+app.put('/card/:id', db.updateCard)
 app.delete('/card/:id', db.deleteCard)
 
 //accessing terminal
 app.get('/terminal', db.getTerminal)
 app.post('/terminal', db.createTerminal)
 app.get('/terminal/:id', db.getTerminalById)
+app.put('/terminal/:id', db.updateTerminal)
 app.delete('/terminal/:id', db.deleteTerminal)
 
 app.listen(port, () => {
