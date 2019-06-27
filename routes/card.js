bc3480992996be6d142ceef4b2cf58c6014e04b5
DimYog <dimasyogapra@gmail.com>
2019-06-27 11:12:47 +0700

    remove getCard auth

--- routes/card.js
+++ routes/card.js
@@ -7,7 +7,7 @@ const auth = require(path.join(__dirname,'..','controllers','auth'))
 /* routes /card */
 router.get('/', auth.checkToken, db.getCard);
 router.get('/:id', auth.checkToken, db.getCardById);
-router.post('/', auth.checkToken, db.createCard);
+router.post('/', db.createCard);
 router.put('/:id', auth.checkToken, db.updateCard);
 router.delete('/:id', auth.checkToken, db.deleteCard);
 
