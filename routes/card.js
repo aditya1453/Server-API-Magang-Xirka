88ba40d4f91d4384afda9d0c6bb321874d665d50
Dimas Yoga <dimasyogapra@gmail.com>
2019-07-04 16:02:13 +0700

    add options

--- routes/card.js
+++ routes/card.js
@@ -5,7 +5,7 @@ const db = require(path.join(__dirname,'..','controllers','queries'))
 const auth = require(path.join(__dirname,'..','controllers','auth'))
 
 /* routes /card */
-router.get('/', auth.checkToken, db.getCard);
+router.options('/', auth.checkToken, db.getCard);
 router.get('/:id', auth.checkToken, db.getCardById);
 router.post('/', db.createCard);
 router.put('/:id', auth.checkToken, db.updateCard);
